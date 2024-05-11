6f2e00f38a27388d7b94931fc6d7c9c30d3efe82
Monali Chavan <monalichavan231@gmail.com>
2024-05-11 22:02:03 +0530

    adding static keyword Program

--- src/oops_concepts/staticKeyword.java
+++ src/oops_concepts/staticKeyword.java
@@ -1,5 +1,13 @@
 package oops_concepts;
 
+/*
+ * Static can be
+ * 1.Variable
+ * 2.Nested Class
+ * 3.Method
+ * 4.Block
+ */
+
 public class staticKeyword {
 	static int num =0;
 	int no = 0;
@@ -15,13 +23,55 @@ public class staticKeyword {
 		System.out.println("Value of non-static variable : "+no);//it stores non-static variable in memory 
 		//everytime instance gets created
 	}
+	
+	static void test() {
+		System.out.println("Static method");
+	}
+	
+	static {
+		System.out.println("This is static block.This will always execute before main method");
+	}
+	
+	//can be overload
+	static void test(int i) {
+		System.out.println("overloaded static method "+i);
+	}
 
+	//main() method is static to avoid create object for it and to save memory, it can be overloaded but
+	//JVM picks the default one while execution
 	public static void main(String[] args) {
 		staticKeyword obj = new staticKeyword();
 		obj.print();
 		staticKeyword obj1 = new staticKeyword();
 		obj1.print();
 		System.out.println(staticKeyword.num);
+		test();
+		test(1);
 	}
+}
 
+class B extends staticKeyword{
+	@Override
+	void print() {
+		System.out.println("hello");
+	}
+	
+	//method overriding will be not allowed as methos is static
+	/*@Override
+	void test(int i) {
+		System.out.println("overloaded static method "+i);
+	}*/
+	
+	//only nested classes can be static in nature
+	static class c{
+		void print() {
+			
+		}
+		static class d extends c{
+			@Override
+			void print() {
+				
+			}
+		}
+	}
 }
